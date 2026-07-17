c7a96d3ff0159bbfdd74f97b28d6b92b025cfb3d
ayorgi <ardasahin7@gmail.com>
2026-07-17 17:00:58 +0300

    fix: add assetPrefix and trailingSlash for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   ...(isProd && { output: 'export' }),
   basePath: isProd ? '/crm-project' : '',
+  assetPrefix: isProd ? '/crm-project/' : '',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
